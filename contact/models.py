eeab078a1b3457b9673bd871bb032e44077ed1f4
FAROUKDEV7 <faroukeldreinydev74@gmail.com>
2024-08-22 04:22:55 +0300

    add contact us and deal with gmail

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Info(models.Model):
+    place=models.CharField(max_length=200)
+    phone_number=models.CharField(max_length=15)
+    email=models.EmailField(max_length=400)
+
+    def __str__(self):
+        return self.email
+    
--- project/settings.py
+++ project/settings.py
@@ -141,3 +141,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # media
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='media/'
+
+
+
+# send email
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='faroukeldreinydev74@gmail.com'
+EMAIL_HOST_PASSWORD='lrai pwyv meoj iivx'
+EMAIL_USE_TLS=True
+EMAIL_PORT='587'
+
+
